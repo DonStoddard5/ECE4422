240fcd16f1edfbceed61e9861682a260b723fe1e
DonStoddard5 <55163173+DonStoddard5@users.noreply.github.com>
2019-09-23 21:21:08 -0400

    Update setup.py

--- DSWServo/setup.py
+++ DSWServo/setup.py
@@ -6,6 +6,7 @@ setup(
     name='DSWServo',
     version='1.0.0',
     description='Python code to control a servo on a Raspberry Pi',
+    url='https://github.com/DonStoddard5/ECE4422/tree/master/DSWServo',
     author='Don Webster',
     author_email='tuj45546@temple.edu',
     packages=find_packages(),
